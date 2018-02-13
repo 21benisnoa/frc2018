91988f6bbb7202178238da1cddf7b741faf0fb6b
Austen <Austen@DESKTOP-KP99138>
2018-02-13 15:42:08 -0600

    Created the FieldCalculations class.

--- jonah/src/org/usfirst/frc/team5903/robot/FieldCalculations.java
+++ jonah/src/org/usfirst/frc/team5903/robot/FieldCalculations.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team5903.robot;
 
-public class FieldCalculations {
+import org.usfirst.frc.team5903.robot.FieldInfo;
 
+public class FieldCalculations {
+int Robotposition;
+int Aliance;
 }
