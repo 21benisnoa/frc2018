a8534ddca0aec1cd74c45e5f954f2ad12d191fa4
Austen <Austen@DESKTOP-KP99138>
2018-02-14 00:25:25 -0600

    added timers

--- jonah/src/org/usfirst/frc/team5903/robot/Autonomouspath.java
+++ jonah/src/org/usfirst/frc/team5903/robot/Autonomouspath.java
@@ -1,26 +1,32 @@
 package org.usfirst.frc.team5903.robot;
 
 import org.usfirst.frc.team5903.robot.FieldCalculations;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Autonomouspath {
 
 	String Pathid;
+	private Timer m_timer = new Timer();
 	private FieldCalculations m_FieldCalculations = new FieldCalculations(); //declares a Field Calculations object so the Pathid can be retrieved
 	
 	
 	public LeftDrive() {//Declares the LeftDrive method
 		Pathid = m_FieldCalculations.Path; // Retrieves the Path id from Field Calculations
 		if (Pathid == "11") {//checks for pathid being 11
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		else if (Pathid == "14") {//checks for pathid being 14
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		else if (Pathid == "22") {//checks for pathid being 22
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		else if (Pathid == "23") {//checks for Pathid being 23
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		return;
 	}
@@ -30,17 +36,21 @@ public class Autonomouspath {
 	public RightDrive() {//declares the Rightdrive method
 		Pathid = m_FieldCalculations.Path; // Retrieves the Path id from Field Calculations
 		if (Pathid == "11") {//checks for pathid being 11
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		else if (Pathid == "14") {//checks for pathid being 14
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		else if (Pathid == "22") {//checks for pathid being 22
-			
+			m_timer.reset();
+			m_timer.start();
 		}
 		else if (Pathid == "23") {//checks for Pathid being 23
-			
-		
+			m_timer.reset();
+			m_timer.start();
+		}
 		return;
 	}
 }
