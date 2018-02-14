b7f173fad7781fe7b2ff357e919ac10a249954fc
Austen <Austen@DESKTOP-KP99138>
2018-02-13 22:55:14 -0600

    Set up base path ID calculations

--- jonah/src/org/usfirst/frc/team5903/robot/FieldCalculations.java
+++ jonah/src/org/usfirst/frc/team5903/robot/FieldCalculations.java
@@ -8,6 +8,8 @@ public class FieldCalculations {
 	String Follow;
 	String m_teamLoc;
 	String Path;
+	String Switch;
+	String Scale;
 	
 	public String Getcolours() {
 		// Get information from the Field Management System (FMS)
@@ -15,10 +17,42 @@ public class FieldCalculations {
 		m_teamLoc = m_teamInfo.getFieldInfo();
 		return m_teamLoc;
 	}
+	public String Pathid() {
+		char Charswitch;
+		char Charscale;
+		Charswitch = m_teamLoc.charAt(1);
+		Charscale = m_teamLoc.charAt(2);
+		String Switch = String.valueOf(Charswitch); //convert Charswitch to Switch, because chars and strings dont like eachother
+		String Scale = String.valueOf(Charscale); //above but with Scale
+		if (Switch == "L") { //check for switch being L
+			if (Scale == "L") { //check scale being L
+				Path = "11";
+			}
+			else if (Scale == "R") {//check scale being R
+				Path = "14";
+			}
+		}
+		else if (Switch == "R") {//check switch being R
+			if (Scale == "L") {//check scale being L
+				Path = "23";
+			}
+			else if (Scale == "R") {//check scale being R
+				Path = "22";
+			}
+		}
+		return Path;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 	public String Leftactions() {
 		String leftActions;
-//		if (m_teamLoc == RLLL) or if (m_teamLoc == BLLL) {
-//		}
+			
 		return leftActions;
 	}
 }
