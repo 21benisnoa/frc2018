2ccb4ba6627caf381d525ca9104daa1c27f9e692
Norse CNC <thorbots5903@gmail.com>
2018-04-05 19:59:08 -0500

    Add Herd sendablechooser option and logic to process the option, add useful comments, tweak the heck out of timing code.

--- jonah/src/org/usfirst/frc/team5903/robot/Robot.java
+++ jonah/src/org/usfirst/frc/team5903/robot/Robot.java
@@ -47,12 +47,16 @@ public class Robot extends IterativeRobot {
 							// second.
 	// For getting Dashboard autonomous choice
 	private String m_autoSelected; // formerly the mode variable
+	private String Herd;
 	private SendableChooser<String> chooser = new SendableChooser<>();
 	private static final String kDefault = "Default";
 	private static final String kLeft = "Auto mode for left position";
 	private static final String kMiddle = "Auto mode for middle position";
 	private static final String kRight = "Auto mode for right position";
 	private String Pathid;
+	private SendableChooser<String> chooser2 = new SendableChooser<>();
+	private static final String kYes = "Herd cubes.";
+	private static final String kNo = "don't herd cubes.";
 
 	/**
 	 * This function is run when the robot is first started up and should be used
@@ -69,7 +73,11 @@ public class Robot extends IterativeRobot {
 		chooser.addObject("Middle", kMiddle);
 		chooser.addObject("Right", kRight);
 		SmartDashboard.putData("Autonomous Selector", chooser);
-
+		SmartDashboard.putData("Herd cubes?", chooser2);
+		chooser2.addObject("Yes", kYes);
+		chooser2.addObject("No", kNo);
+		chooser2.addDefault("Default", kDefault);
+		
 		// Starts camera feeds
 		cam0 = CameraServer.getInstance().startAutomaticCapture(0);
 		cam1 = CameraServer.getInstance().startAutomaticCapture(1);
@@ -94,6 +102,12 @@ public class Robot extends IterativeRobot {
 			m_autoSelected = kDefault;
 			System.out.println("Shuffleboard error, did you forget to start the shuffleboard?");
 		}
+		try {
+			Herd = chooser2.getSelected(); //Get herding yes/no from shuffleboard
+		} catch (Exception e) {
+			Herd = kDefault;
+			System.out.println("Shuffleboard error, did you forget to start the shuffleboard?");
+		}
 		System.out.println("autonomous init: auto selected is: " + m_autoSelected);
 		HasLoc = 1; // sets HasLoc to one because we have retrieved the location from the
 		// smartdashboard by now.
@@ -108,108 +122,137 @@ public class Robot extends IterativeRobot {
 			System.out.println("Position 1 selected, we are left side.");
 			if (Pathid == "11" || Pathid == "21") {
 				System.out.println("Path ID 11 confirmed, the Switch and Scale are Left.");
+				Control.Openclaw();
+				Timer.delay(.2);
+				Control.Closeclaw();
+				Timer.delay(.2);
 				Control.Raisearm();
 				Control.Closeclaw();
-				Timer.delay(1); // Raises arm for a second
+				Timer.delay(.6); // Raises arm for a second
+				Control.Forwards(.75);
+				Timer.delay(1.9); // moves robot forwards until it drifts
+				Control.Right(.6);
+				Control.Stoparm(); 
+				Timer.delay(0.4);// counters drift
+				Control.Forwards(0.75);
+				Timer.delay(1.7);// moves robot forwards again
+//stop arm was here
+				Timer.delay(0.4); // stops arm so it doesnt raise too high
+				Control.Right(.7);
+				Control.Stopclaw();
+				Timer.delay(1);// turns robot right so we can put a cube on the scale
+				Control.Forwards(.6);
+				Timer.delay(0.3);// moves robot forwards so the cube is above the scale
+				Control.Stop();
+				Timer.delay(.3);// stops robot so we don't run into the scale
+				Control.Openclaw();
+				Timer.delay(0.1);// opens the claw to drop the cube
+				Control.Backwards(.7);
+				Timer.delay(.4);// moves robot backwards so we don't end up stuck on the scale
+				Control.Stop();
+			} // END PATHID 11 CODE
+			else if (Pathid == "12" || Pathid == "22") {
+				//System.out.println("Path ID 14 confirmed, the Switch is Left and the Scale is Right.");
+				Control.Openclaw();
+				Timer.delay(.2);
+				Control.Closeclaw();
+				Timer.delay(.2);
+				Control.Raisearm();
+				Control.Closeclaw();
+				Timer.delay(.6); // Raises arm for a second
 				Control.Forwards(.75);
 				Timer.delay(1.9); // moves robot forwards until it drifts
 				Control.Right(.6);
 				Timer.delay(0.4);// counters drift
 				Control.Forwards(0.75);
+				Control.Stoparm();
 				Timer.delay(1.7);// moves robot forwards again
-				Control.Stoparm();
-				Timer.delay(0.4); // stops arm so it doesnt raise too high
-				Control.Right(.7);
-				Control.Stopclaw();
-				Timer.delay(1);// turns robot right so we can put a cube on the scale
-				Control.Forwards(.7);
-				Timer.delay(0.4);// moves robot forwards so the cube is above the scale
-				Control.Stop();
-				Timer.delay(.3);// stops robot so we don't run into the scale
-				Control.Openclaw();
-				Control.Release(1);
-				Timer.delay(0.1);// opens the claw to drop the cube
-				Control.Backwards(.7);
-				Timer.delay(.4);// moves robot backwards so we don't end up stuck on the scale
-				Control.Stop();
-				Control.StopGrabber();
-			} // END PATHID 11 CODE
-			else if (Pathid == "12" || Pathid == "22") {
-				System.out.println("Path ID 14 confirmed, the Switch is Left and the Scale is Right.");
-				Control.Raisearm();
-				Control.Closeclaw();
-				Timer.delay(1);// Raises arm for a second before we start moving, so that it doesn't drag on
-								// the floor
-				Control.Forwards(.75);
-				Timer.delay(1.9);// Moves the robot forwards for 1.9 seconds, until the robot drifts too far to
-									// the left and we have to correct.
-				Control.Right(.6);
-				Timer.delay(0.4);// turns the robot right for .4 seconds, this is to correct for the drift
-				Control.Forwards(0.75);
-				Timer.delay(1);// moves the robot forwards again
-				Control.Stoparm();
-				Control.Right(.7);
-				Control.Stopclaw();
-				Timer.delay(1.3);// turns the robot right so we can push cubes,also stops the arm so it doesn't
-									// raise too high
-				Control.Forwards(.6);
-				Timer.delay(1.5);// moves the robot forwards so we can push cubes, until it drifts too far to the
-									// left again.
-				Control.Right(.7);
-				Timer.delay(.1);// turns the robot right to counter drift again
-				Control.Forwards(.6);
-				Timer.delay(1.5);// moves the robot forwards once more and then brings it to a stop
-				Control.Stop();
+				switch (Herd) {
+				case kYes:
+					System.out.println("Herd is yes");
+					Control.Right(.7);
+					Control.Stopclaw();
+					Timer.delay(1.3);// turns the robot right so we can push cubes,also stops the arm so it doesn't
+					// raise too high
+					Control.Forwards(.6);
+					Timer.delay(1.5);// moves the robot forwards so we can push cubes, until it drifts too far to the
+					// left again.
+					Control.Right(.7);
+					Timer.delay(.1);// turns the robot right to counter drift again
+					Control.Forwards(.6);
+					Timer.delay(1.5);// moves the robot forwards once more and then brings it to a stop
+					Control.Stop();
+					break;
+				case kNo:
+					Control.Stopclaw();
+					Control.Stop();
+					System.out.println("Herd is no");
+				default:
+					Control.Stop();
+					System.out.println("Herd protocol undefined, assuming no.");
+					break;
+				}
 			} // END PATHID 14
-				// End LEFT position code
+			// End LEFT position code
 			break;
 		case kMiddle:
 			System.out.println("Position 2 selected, we are in the middle.");
 			if (Pathid == "11" || Pathid == "12") {
+				System.out.println("The Switch is left, performing left path code");
 				Control.Raisearm();
 				Control.Forwards(.6);
 				Control.Closeclaw();
-				Timer.delay(0.7);
+				System.out.println("Moving forwards...");
+				Timer.delay(0.6);
 				Control.Left(.6);
 				Control.Stopclaw();
+				System.out.println("Turning Left...");
 				Timer.delay(0.6);
 				Control.Forwards(.7);
+				System.out.println("Moving forwards...");
 				Timer.delay(1);
 				Control.Stoparm();
 				Timer.delay(0.4);
 				Control.Right(.7);
+				System.out.println("turning right...");
 				Timer.delay(0.6);
 				Control.Forwards(.6);
+				System.out.println("Moving forwards to Switch...");
 				Timer.delay(0.3);
 				Control.Openclaw();
-				Control.Release(.5);
+				System.out.println("Dropping cube");
 				Timer.delay(.1);
 				Control.Stopclaw();
-				Control.StopGrabber();
 				Control.Stop();
-				Control.StopGrabber();
+				System.out.println("Path Complete.");
 			} else if (Pathid == "22" || Pathid == "21") {
+				System.out.println("The Switch is right, performing right path code");
 				Control.Forwards(.6);
 				Control.Closeclaw();
 				Control.Raisearm();
-				Timer.delay(0.7);
+				System.out.println("Moving forwards...");
+				Timer.delay(0.6);
 				Control.Right(.6);
 				Control.Stopclaw();
+				System.out.println("Turning right...");
 				Timer.delay(0.6);
 				Control.Forwards(.7);
+				System.out.println("Moving forwards...");
 				Timer.delay(1);
 				Control.Stoparm();
 				Timer.delay(0.3);
 				Control.Left(0.7);
-				Timer.delay(0.4);
+				System.out.println("Turning left...");
+				Timer.delay(0.6);
 				Control.Forwards(.6);
+				System.out.println("Moving forwards to Switch.");
 				Timer.delay(0.3);
 				Control.Openclaw();
-				Control.Release(.5);
+				System.out.println("Dropping cube.");
 				Timer.delay(0.1);
 				Control.Stopclaw();
 				Control.Stop();
-				Control.StopGrabber();
+				System.out.println("Path Complete.");
 			}
 			break;
 
@@ -225,63 +268,90 @@ public class Robot extends IterativeRobot {
 		case kRight:
 			System.out.println("Position 3 selected, we are right side.");
 			if (Pathid == "11" || Pathid == "21") {
-				// move forward and grip cube
+				Control.Openclaw();
+				Timer.delay(.2);
+				Control.Closeclaw();
+				Timer.delay(.2);
 				Control.Raisearm();
 				Control.Closeclaw();
-				Timer.delay(1);
+				Timer.delay(.6); // Raises arm for a second
 				Control.Forwards(.75);
-				Timer.delay(1.9);
+				Timer.delay(1.9); // moves robot forwards until it drifts
 				Control.Right(.6);
-				Timer.delay(0.4);
+				Timer.delay(0.4);// counters drift
 				Control.Forwards(0.75);
-				Timer.delay(1);
 				Control.Stoparm();
-				Control.Left(.7);
+				Timer.delay(1.7);// moves robot forwards again
 				Control.Stopclaw();
-				Timer.delay(1.3);
-				Control.Forwards(.6);
-				Timer.delay(1.5);
-				Control.Right(.7);
-				Timer.delay(.1);
-				Control.Forwards(.6);
-				Timer.delay(1.5);
-				Control.Stop();
+				System.out.println("stopping arm.");
+				switch (Herd) {
+				case kYes:
+					System.out.println("Herd is yes");
+					Control.Left(.7);
+					System.out.println("Turning left to face cubes.");
+					Timer.delay(1.3);
+					Control.Forwards(.6);
+					System.out.println("Herding cubes...");
+					Timer.delay(1.5);
+					Control.Right(.7);
+					System.out.println("Countering drift...");
+					Timer.delay(.1);
+					Control.Forwards(.6);
+					System.out.println("Continuing to herd cubes...");
+					Timer.delay(1.5);
+					Control.Stop();
+					System.out.println("Cubes herded, path complete.");
+					break;
+				case kNo:
+					Control.Stop();
+					System.out.println("Herd is no, path complete.");
+					break;
+				default:
+					Control.Stop();
+					System.out.println("Herd protocol undefined, assuming no.");
+					System.out.println("Path complete.");
+					break;
+				}
 			} // END PATHID 11 CODE
 			else if (Pathid == "12" || Pathid == "22") {
-				// move forward and grip cube
+				System.out.println("The Scale is right, putting cube on Scale.");
+				Control.Openclaw();
+				Timer.delay(.2);
+				Control.Closeclaw();
+				Timer.delay(.2);
 				Control.Raisearm();
 				Control.Closeclaw();
-				Timer.delay(1);
+				Timer.delay(.6); // Raises arm for a second
 				Control.Forwards(.75);
-				Timer.delay(1.9);
+				Timer.delay(1.9); // moves robot forwards until it drifts
 				Control.Right(.6);
-				Timer.delay(0.4);
-				Control.Forwards(0.75);
-				Timer.delay(1.7);
 				Control.Stoparm();
-				Timer.delay(0.4);
+				Timer.delay(0.4);// counters drift
+				Control.Forwards(0.75);
+				Timer.delay(1.7);// moves robot forwards again
+				//StopArm was here
+				Timer.delay(0.4); // stops arm so it doesnt raise too high
 				Control.Left(.7);
 				Control.Stopclaw();
-				Timer.delay(1);
-				Control.Forwards(.7);
-				Timer.delay(0.4);
+				Timer.delay(1);// turns robot right so we can put a cube on the scale
+				Control.Forwards(.6);
+				Timer.delay(0.3);// moves robot forwards so the cube is above the scale
 				Control.Stop();
-				Timer.delay(.3);
+				Timer.delay(.3);// stops robot so we don't run into the scale
 				Control.Openclaw();
-				Control.Release(1);
-				Timer.delay(0.1);
+				Timer.delay(0.1);// opens the claw to drop the cube
 				Control.Backwards(.7);
-				Timer.delay(.4);
+				Timer.delay(.4);// moves robot backwards so we don't end up stuck on the scale
 				Control.Stop();
-				Control.StopGrabber();
 			} // END PATHID 14
 				// End RIGHT position code
 			break;
 		case kDefault:
 		default:
 			System.out.println("You messed up, Bad. Going to backup path.");
-			Control.Forwards(.7);
 			Control.Raisearm();
+			Timer.delay(.6);
+			Control.Forwards(.7);
 			Control.Closeclaw();
 			Timer.delay(2);
 			Control.Stop();
@@ -321,7 +391,8 @@ public class Robot extends IterativeRobot {
 		Control.Triggercontrol();
 		Control.Clawcontrol();
 		Control.Climbcontrol();
-		Control.Grabbercontrol();
+		Control.Punch();
+		
 	}
 
 	@Override
